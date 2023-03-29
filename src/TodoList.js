8fcfd4dc214973821fde202c4bf8ef76731c7b23
Saurabh Panja <saurabh.panja@phamax.ch>
2023-03-29 06:34:46 +0530

    can check and uncheck a task

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "./TodoList.css";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
-  const [newTodoTitle, setNewTodoTitle] = useState('');
+  const [newTodoTitle, setNewTodoTitle] = useState("");
   const [editedTodo, setEditedTodo] = useState(null);
 
   useEffect(() => {
@@ -14,7 +14,7 @@ const TodoList = () => {
 
   const fetchTodos = async () => {
     const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10'
+      "https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5"
     );
     setTodos(response.data);
   };
@@ -23,14 +23,14 @@ const TodoList = () => {
     const newTodo = {
       title: newTodoTitle,
       completed: false,
-      userId: 1 // This is a required field for the API
+      userId: 1, // This is a required field for the API
     };
     const response = await axios.post(
-      'https://jsonplaceholder.typicode.com/todos',
+      "https://jsonplaceholder.typicode.com/todos",
       newTodo
     );
     setTodos([...todos, response.data]);
-    setNewTodoTitle('');
+    setNewTodoTitle("");
   };
 
   const editTodo = (id, updates) => {
@@ -40,14 +40,13 @@ const TodoList = () => {
   };
 
   const updateTodo = async (id, updates) => {
-    const response = await axios.put(
+    await axios.put(
       `https://jsonplaceholder.typicode.com/todos/${id}`,
       updates
     );
-
+    console.log(updates);
     setEditedTodo(null);
   };
-
 
   const deleteTodo = async (id) => {
     await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
@@ -62,36 +61,73 @@ const TodoList = () => {
           e.preventDefault();
           addTodo();
         }}
+        className="add-todo-form"
       >
         <input
           type="text"
           placeholder="Add a new todo"
           value={newTodoTitle}
           onChange={(e) => setNewTodoTitle(e.target.value)}
+          className="todo-input"
         />
-        <button>Add</button>
+        <button className="todo-btn add-todo-btn">Add</button>
       </form>
-      <ul>
+      <ul className="all-todo-list">
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li key={todo.id} className="todo-list-item">
             {editedTodo === todo.id ? (
               <input
                 type="text"
                 value={todo.title}
                 onChange={(e) =>
-                  editTodo(todo.id, { title: e.target.value, completed: todo.completed })
+                  editTodo(todo.id, {
+                    title: e.target.value,
+                    completed: todo.completed,
+                  })
                 }
-                onBlur={() => updateTodo(todo.id, { title: todo.title, completed: todo.completed })}
+                onBlur={() =>
+                  updateTodo(todo.id, {
+                    title: todo.title,
+                    completed: todo.completed,
+                  })
+                }
+                className="todo-input edit-todo-input"
               />
             ) : (
               <>
-                <span
-                  className={todo.completed ? 'completed' : ''}
+                <div
+                  className={
+                    todo.completed
+                      ? "edit-todo-input completed"
+                      : "edit-todo-input"
+                  }
                   onClick={() => setEditedTodo(todo.id)}
                 >
                   {todo.title}
-                </span>
-                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                </div>
+                <div className="todo-btn-container">
+                  <button
+                    className={
+                      todo.completed
+                        ? "todo-btn uncheck-todo-btn p-05"
+                        : "todo-btn check-todo-btn p-05"
+                    }
+                    onClick={() => {
+                      editTodo(todo.id, {
+                        title: todo.title,
+                        completed: !todo.completed,
+                      });
+                    }}
+                  >
+                    {todo.completed ? "Uncheck" : "Check"}
+                  </button>
+                  <button
+                    className="todo-btn delete-todo-btn p-05"
+                    onClick={() => deleteTodo(todo.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
               </>
             )}
           </li>
